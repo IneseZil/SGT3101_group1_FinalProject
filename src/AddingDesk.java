1c97cdebed01ef575df338bc17e5838a7432aeef
IneseZil <IneseZil@github.com>
2022-09-14 21:59:24 +0300

    added AddDesk and BookDesk class

---
+++ src/AddingDesk.java
@@ -0,0 +1,36 @@
+public class AddingDesk {
+    private String wplaceID;
+    private int floor;
+    private int room;
+    private int deskID;
+
+    public void setWplaceID(String wplaceID) {
+        this.wplaceID = wplaceID;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public void setDeskID(int deskID) {
+        this.deskID = deskID;
+    }
+
+    public String getWplaceID() {
+        return wplaceID;
+    }
+    public int getFloor () {
+        return floor;
+    }
+    public int getRoom () {
+        return room;
+    }
+    public int getDeskID () {
+        return deskID;
+    }
+
+}
---
+++ src/BookingDesk.java
@@ -0,0 +1,16 @@
+public class BookingDesk {
+
+    private char occupied;
+    private String dateFrom;
+    private String dateTo;
+
+    public void setOccupied(char occupied) {
+        this.occupied = occupied;
+    }
+    public void setDateFrom(String dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+    public void setDateTo(String dateTo) {
+        this.dateTo = dateTo;
+    }
+}
